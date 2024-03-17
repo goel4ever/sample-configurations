c49802d53426acf43388d7058403f84e49a9de6c
Anshul Goel <dec.anshul@gmail.com>
2024-03-17 15:45:10 -0400

    cassandra docker-compose

---
+++ docker-compose-database/cassandra/README.md
@@ -0,0 +1,3 @@
+# Cassandra Docker Compose
+
+Ready-to-use docker compose file for Cassandra.
---
+++ docker-compose-database/cassandra/configs/cassandra-env.sh
@@ -0,0 +1,4 @@
+# The amount of memory to allocate to the JVM heap for Cassandra
+MAX_HEAP_SIZE="2G"
+# The smaller of 1/2 of RAM or 1024MB
+HEAP_NEWSIZE="1G"
